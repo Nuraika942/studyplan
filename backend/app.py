edabddb209d1a51424727c0206b5b4dcecda983a
Nuraika942 <nurajkamcybekova241@gmail.com>
2026-06-17 01:53:03 +0600

    Update app.run to use environment PORT variable

--- backend/app.py
+++ backend/app.py
@@ -143,4 +143,5 @@ def delete_task(task_id):
     return jsonify({'message': 'Задача удалена'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+       port = int(os.environ.get("PORT", 5000))
+       app.run(host='0.0.0.0', port=port, debug=True)
